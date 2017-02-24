edeacbdcb045f0671fc847fecccb24ba067b8217
MichaelTd <tsouchlarakis@gmail.com>
2017-02-24 04:31:33 +0200

    Update tkRootMenu.py

--- tkRootMenu.py
+++ tkRootMenu.py
@@ -6,15 +6,9 @@ import os
 from Tkinter import *
 #print os.name
 
-__terminal__ = "terminology"
 #__terminal__ = "xfce4-terminal --disable-server --geometry=120x40"
-#__editor__ = "vscode"
-#__editor__ = "sublime"
-#__editor__ = "light"
+__terminal__ = "terminology"
 __editor__ = "atom"
-#__editor__ = "emacs"
-#__editor__ = "gvim"
-#__file_manager__ = "gentoo"
 __file_manager__ = "xfe"
 __browser__ = "firefox"
 #__sudo_cmd__ = "SUDO_ASKPASS=/usr/bin/ssh-askpass-fullscreen &&sudo --askpass "
@@ -22,7 +16,6 @@ __sudo_cmd__ = "SUDO_ASKPASS=/usr/bin/x11-ssh-askpass &&sudo --askpass "
 
 def runCommand(app):
   os.system(app + " &")
-
 
 # Default Apps
 def on_accel_runTerminal(widget):
@@ -72,126 +65,126 @@ root.bind_all("<Control-e>", on_accel_runEditor)
 
 menubar = Menu(root)
 
-devmenu=Menu(menubar)
-netmenu=Menu(menubar)
-mmmenu=Menu(menubar)
-gammenu=Menu(menubar)
-configmenu=Menu(menubar)
-utilmenu=Menu(menubar)
-toolsmenu=Menu(menubar)
-adminmenu=Menu(menubar)
-appsmenu=Menu(menubar)
+devmenu = Menu(menubar)
+netmenu = Menu(menubar)
+mmmenu = Menu(menubar)
+gammenu = Menu(menubar)
+configmenu = Menu(menubar)
+utilmenu = Menu(menubar)
+toolsmenu = Menu(menubar)
+adminmenu = Menu(menubar)
+appsmenu = Menu(menubar)
 
 # Dev menu
 for lbl, cmmnd in (("Eclipse", "runeclipse.sh"),
-                  ("Netbeans", "netbeans"),
-                  ("KDevelop", "kdevelop"),
-                  ("QtCreator", "qtcreator.sh"),
-                  ("QtDesigner", "qtchooser -run-tool=designer -qt=5"),
-                  ("Glade", "glade"),
-                  ("GVim", "gvim"),
-                  ("Vim", __terminal__ + " -e vim"),
-                  ("Diakonos", __terminal__ + " -e diakonos"),
-                  ("Xemacs", __terminal__ + " -e xemacs"),
-                  ("Emacs", "emacs"),
-                  ("ZED", __terminal__ + " -e /bin/env /bin/bash ~/opt/zed/zed"),
-                  ("Sublime Text", "sublime"),
-                  ("LightTable", "light"),
-                  ("Atom", "atom")):
-  devmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+    ("Netbeans", "netbeans"),
+    ("KDevelop", "kdevelop"),
+    ("QtCreator", "qtcreator.sh"),
+    ("QtDesigner", "qtchooser -run-tool=designer -qt=5"),
+    ("Glade", "glade"),
+    ("GVim", "gvim"),
+    ("Vim", __terminal__ + " -e vim"),
+    ("Diakonos", __terminal__ + " -e diakonos"),
+    ("Xemacs", __terminal__ + " -e xemacs"),
+    ("Emacs", "emacs"),
+    ("ZED", __terminal__ + " -e /bin/env /bin/bash ~/opt/zed/zed"),
+    ("Sublime Text", "sublime"),
+    ("LightTable", "light"),
+    ("Atom", "atom")):
+  devmenu.add_command(label = lbl,command = lambda param = cmmnd: runCommand(param))
 
 # Internet
 for lbl, cmmnd in (("Firefox", "firefox"),
-                  ("Firefox DE", "firefox-de"),
-                  ("Mozilla", "mozilla"),
-                  ("FileZilla", "filezilla"),
-                  ("HexChat", "hexchat")):
-  netmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Firefox DE", "firefox-de"),
+  ("Mozilla", "mozilla"),
+  ("FileZilla", "filezilla"),
+  ("HexChat", "hexchat")):
+  netmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 # Multimedia
 for lbl, cmmnd in (("Gimp", "gimp"),
-                  ("Open Shot", "openshot"),
-                  ("Audacious", "audacious"),
-                  ("VLC", "vlc"),
-                  ("Open Office", "ooffice"),
-                  ("Libre Office", "/opt/libreOffice"),
-                  ("Foxit Reader", "~/bin/FoxitReader"),
-                  ("Qpdf Viewer", "qpdfview"),
-                  ("Ghost View", "gv")):
-  mmmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Open Shot", "openshot"),
+  ("Audacious", "audacious"),
+  ("VLC", "vlc"),
+  ("Open Office", "ooffice"),
+  ("Libre Office", "/opt/libreOffice"),
+  ("Foxit Reader", "~/bin/FoxitReader"),
+  ("Qpdf Viewer", "qpdfview"),
+  ("Ghost View", "gv")):
+  mmmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 # Games menu
 for lbl, cmmnd in (("Quake 3", "~/bin/ioq3"),
-                  ("Quake 3 TA", "~/bin/ioq3-ta"),
-                  ("Urban Terror", "~/bin/ut42u"),
-                  ("Warsow", "~/warsow_21/warsow")):
-  gammenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Quake 3 TA", "~/bin/ioq3-ta"),
+  ("Urban Terror", "~/bin/ut42u"),
+  ("Warsow", "~/warsow_21/warsow")):
+  gammenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 # Config Menu
 for lbl, cmmnd in (("Compiz settings manager", "ccsm"),
-                  ("Emerald themes manager", "emerald-theme-manager"),
-                  ("Conky config", __editor__ + " ~/.conky.conf/.conkyrc.left ~/.conky.conf/.conkyrc.middle ~/.conky.conf/.conkyrc.right ~/.conky.conf/.conkyrc.lxde.my ~/.conky.conf/.conkyrc.right.full"),
-                  ("Edit backup files", __editor__ + " ~/.backup.files.txt ~/.backup.themes.txt ~/.backup.git.txt"),
-                  ("View Log files", __editor__ + " /var/log/update.perl.log /var/log/rsync.data.log /var/log/portage.emerge.log /var/log/backup-paperjam.log"),
-                  ("X Screen Saver", "xscreensaver-demo"),
-                  ("Alsamixer", __terminal__ + " -e alsamixer")):
-  configmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Emerald themes manager", "emerald-theme-manager"),
+  ("Conky config", __editor__ + " ~/.conky.conf/"),
+  ("Edit backup files", __editor__ + " ~/.backup.*"),
+  ("View Log files", __editor__ + " /var/log/portage.emerge.log"),
+  ("X Screen Saver", "xscreensaver-demo"),
+  ("Alsamixer", __terminal__ + " -e alsamixer")):
+  configmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 # Util Menu
 for lbl, cmmnd in (("App Runner", "runcmd.sh"),
-                  ("Xfce4 App Finder", "xfce4-appfinder -c --disable-server"),
-                  ("Xarchiver", "xarchiver"),
-                  ("PeaZip", "peazip"),
-                  ("Viewnior", "viewnior"),
-                  ("Xfce4 Screenshot", "xfce4-screenshooter"),
-                  ("Take a shot now", "~/bin/imss.sh 2"),
-                  ("Calculator", "calculator"),
-                  ("jCalculator", "jCalculator.sh"),
-                  ("Javascript Calculator", __browser__ + " ~/git/fcc-app/01-front-end-cert/07-javascript-calculator/jc.html")):
-  utilmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Xfce4 App Finder", "xfce4-appfinder -c --disable-server"),
+  ("Xarchiver", "xarchiver"),
+  ("PeaZip", "peazip"),
+  ("Viewnior", "viewnior"),
+  ("Xfce4 Screenshot", "xfce4-screenshooter"),
+  ("Take a shot now", "~/bin/imss.sh 2"),
+  ("Calculator", "calculator"),
+  ("jCalculator", "jCalculator.sh"),
+  ("Javascript Calculator", __browser__ + " ~/git/fcc-app/01-front-end-cert/07-javascript-calculator/jc.html")):
+  utilmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 # File system tools
 for lbl, cmmnd in (("Terminology", "terminology"),
-                  ("Xfce4 Terminal", "xfce4-terminal --disable-server --geometry=120x40"),
-                  ("URXVT", "urxvt"),
-                  ("Midnight Commander", __terminal__ + " -e mc"),
-                  ("Gentoo", "gentoo"),
-                  ("Xfe", "xfe"),
-                  ("Thunar", "thunar")):
-  toolsmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Xfce4 Terminal", "xfce4-terminal --disable-server --geometry=120x40"),
+  ("URXVT", "urxvt"),
+  ("Midnight Commander", __terminal__ + " -e mc"),
+  ("Gentoo", "gentoo"),
+  ("Xfe", "xfe"),
+  ("Thunar", "thunar")):
+  toolsmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 # Admin tools
 for lbl, cmmnd in (("Terminal", __sudo_cmd__ + " " + __terminal__),
-                  ("Text Editor", __sudo_cmd__ + " " + __editor__),
-                  ("File manager", __sudo_cmd__ + " " + __terminal__ + " -e mc"),
-                  ("Porthole", __sudo_cmd__ + " porthole"),
-                  ("DStat", __sudo_cmd__ + " " + __terminal__ + " -e dstat -fcdngy"),
-                  ("Glances", __sudo_cmd__ + " " + __terminal__ + " -e glances"),
-                  ("PowerTop", __sudo_cmd__ + " " + __terminal__ + " -e powertop"),
-                  ("HTop", __sudo_cmd__ + " " + __terminal__ + " -e htop"),
-                  ("Top", __sudo_cmd__ + " " + __terminal__ + " -e top")):
-  adminmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Text Editor", __sudo_cmd__ + " " + __editor__),
+  ("File manager", __sudo_cmd__ + " " + __terminal__ + " -e mc"),
+  ("Porthole", __sudo_cmd__ + " porthole"),
+  ("DStat", __sudo_cmd__ + " " + __terminal__ + " -e dstat -fcdngy"),
+  ("Glances", __sudo_cmd__ + " " + __terminal__ + " -e glances"),
+  ("PowerTop", __sudo_cmd__ + " " + __terminal__ + " -e powertop"),
+  ("HTop", __sudo_cmd__ + " " + __terminal__ + " -e htop"),
+  ("Top", __sudo_cmd__ + " " + __terminal__ + " -e top")):
+  adminmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 menubar.add_cascade(label="Root Menu", menu=appsmenu)
 
 # Basic apps
 for lbl, cmmnd, k in (("Terminal", __terminal__, "ctl+t"),
-                  ("Editor", __editor__, "ctl+e"),
-                  ("File Manager", __file_manager__, "ctl+f"),
-                  ("Browser", __browser__, "ctl+b")):
-  appsmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command), accelerator=k)
+  ("Editor", __editor__, "ctl+e"),
+  ("File Manager", __file_manager__, "ctl+f"),
+  ("Browser", __browser__, "ctl+b")):
+  appsmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param), accelerator=k)
 
 appsmenu.add_separator()
 
 # Groups
 for lbl, mnGrp in (("Development", devmenu),
-                  ("Internet", netmenu),
-                  ("MultiMedia", mmmenu),
-                  ("Games", gammenu),
-                  ("Utilities", utilmenu),
-                  ("Config", configmenu),
-                  ("File Managers", toolsmenu),
-                  ("Admin Tools", adminmenu)):
+  ("Internet", netmenu),
+  ("MultiMedia", mmmenu),
+  ("Games", gammenu),
+  ("Utilities", utilmenu),
+  ("Config", configmenu),
+  ("File Managers", toolsmenu),
+  ("Admin Tools", adminmenu)):
   appsmenu.add_cascade(label=lbl,menu=mnGrp)
 
 appsmenu.add_separator()
@@ -205,9 +198,9 @@ appsmenu.add_separator()
 
 # PC
 for lbl, cmmnd in (("Logout", "kill -15 -1"),
-                  ("Reboot", __sudo_cmd__ + " shutdown -r now"),
-                  ("Shutdown", __sudo_cmd__ + " shutdown -h now")):
-  appsmenu.add_command(label=lbl,command=lambda command=cmmnd: runCommand(command))
+  ("Reboot", __sudo_cmd__ + " shutdown -r now"),
+  ("Shutdown", __sudo_cmd__ + " shutdown -h now")):
+  appsmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
 root.config(menu=menubar)
 
