f26a9eebb55b8b8a046c2450ca0b2e56a72f7dc1
MichaelTd <tsouchlarakis@gmail.com>
2017-03-09 12:24:13 +0200

    Update gtk2RootMenu.py

--- gtk2RootMenu.py
+++ gtk2RootMenu.py
@@ -1,95 +1,102 @@
 #!/usr/bin/env python
 
-# example itemfactory.py
+# example menu.py
 
 import pygtk
 pygtk.require('2.0')
 import gtk
 
-class ItemFactoryExample:
-    # Obligatory basic callback
-    def print_hello(self, w, data):
-        print "Hello, World!"
+class MenuExample:
+  def __init__(self):
+    # create a new window
+    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    window.set_size_request(200, 100)
+    window.set_title("GTK Menu Test")
+    window.connect("delete_event", lambda w,e: gtk.main_quit())
 
-    # This is the ItemFactoryEntry structure used to generate new menus.
-    # Item 1: The menu path. The letter after the underscore indicates an
-    #         accelerator key once the menu is open.
-    # Item 2: The accelerator key for the entry
-    # Item 3: The callback.
-    # Item 4: The callback action.  This changes the parameters with
-    #         which the callback is called.  The default is 0.
-    # Item 5: The item type, used to define what kind of an item it is.
-    #       Here are the possible values:
+    # Init the menu-widget, and remember -- never
+    # show() the menu widget!!
+    # This is the menu that holds the menu items, the one that
+    # will pop up when you click on the "Root Menu" in the app
+    menu = gtk.Menu()
 
-    #       NULL               -> "<Item>"
-    #       ""                 -> "<Item>"
-    #       "<Title>"          -> create a title item
-    #       "<Item>"           -> create a simple item
-    #       "<CheckItem>"      -> create a check item
-    #       "<ToggleItem>"     -> create a toggle item
-    #       "<RadioItem>"      -> create a radio item
-    #       <path>             -> path of a radio item to link against
-    #       "<Separator>"      -> create a separator
-    #       "<Branch>"         -> create an item to hold sub items (optional)
-    #       "<LastBranch>"     -> create a right justified branch 
+    # Next we make a little loop that makes three menu-entries for
+    # "test-menu".  Notice the call to gtk_menu_append.  Here we are
+    # adding a list of menu items to our menu.  Normally, we'd also
+    # catch the "clicked" signal on each of the menu items and setup a
+    # callback for it, but it's omitted here to save space.
+    for i in range(3):
+      # Copy the names to the buf.
+      buf = "Test-undermenu - %d" % i
 
-    def get_main_menu(self, window):
-        accel_group = gtk.AccelGroup()
+      # Create a new menu-item with a name...
+      menu_items = gtk.MenuItem(buf)
 
-        # This function initializes the item factory.
-        # Param 1: The type of menu - can be MenuBar, Menu,
-        #          or OptionMenu.
-        # Param 2: The path of the menu.
-        # Param 3: A reference to an AccelGroup. The item factory sets up
-        #          the accelerator table while generating menus.
-        item_factory = gtk.ItemFactory(gtk.MenuBar, "<main>", accel_group)
+      # ...and add it to the menu.
+      menu.append(menu_items)
 
-        # This method generates the menu items. Pass to the item factory
-        #  the list of menu items
-        item_factory.create_items(self.menu_items)
+      # Do something interesting when the menuitem is selected
+      menu_items.connect("activate", self.menuitem_response, buf)
 
-        # Attach the new accelerator group to the window.
-        window.add_accel_group(accel_group)
+      # Show the widget
+      menu_items.show()
 
-        # need to keep a reference to item_factory to prevent its destruction
-        self.item_factory = item_factory
-        # Finally, return the actual menu bar created by the item factory.
-        return item_factory.get_widget("<main>")
+    # This is the root menu, and will be the label
+    # displayed on the menu bar.  There won't be a signal handler attached,
+    # as it only pops up the rest of the menu when pressed.
+    root_menu = gtk.MenuItem("Root Menu")
 
-    def __init__(self):
-        self.menu_items = (
-            ( "/_File",         None,         None, 0, "<Branch>" ),
-            ( "/File/_New",     "<control>N", self.print_hello, 0, None ),
-            ( "/File/_Open",    "<control>O", self.print_hello, 0, None ),
-            ( "/File/_Save",    "<control>S", self.print_hello, 0, None ),
-            ( "/File/Save _As", None,         None, 0, None ),
-            ( "/File/sep1",     None,         None, 0, "<Separator>" ),
-            ( "/File/Quit",     "<control>Q", gtk.main_quit, 0, None ),
-            ( "/_Options",      None,         None, 0, "<Branch>" ),
-            ( "/Options/Test",  None,         None, 0, None ),
-            ( "/_Help",         None,         None, 0, "<LastBranch>" ),
-            ( "/_Help/About",   None,         None, 0, None ),
-            )
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.connect("destroy", lambda w: gtk.main_quit())
-        window.set_title("Item Factory")
-        window.set_size_request(300, 200)
+    root_menu.show()
 
-        main_vbox = gtk.VBox(False, 1)
-        main_vbox.set_border_width(1)
-        window.add(main_vbox)
-        main_vbox.show()
+    # Now we specify that we want our newly created "menu" to be the
+    # menu for the "root menu"
+    root_menu.set_submenu(menu)
 
-        menubar = self.get_main_menu(window)
+    # A vbox to put a menu and a button in:
+    vbox = gtk.VBox(False, 0)
+    window.add(vbox)
+    vbox.show()
 
-        main_vbox.pack_start(menubar, False, True, 0)
-        menubar.show()
-        window.show()
+    # Create a menu-bar to hold the menus and add it to our main window
+    menu_bar = gtk.MenuBar()
+    vbox.pack_start(menu_bar, False, False, 2)
+    menu_bar.show()
+
+    # Create a button to which to attach menu as a popup
+    button = gtk.Button("press me")
+    button.connect_object("event", self.button_press, menu)
+    vbox.pack_end(button, True, True, 2)
+    button.show()
+
+    # And finally we append the menu-item to the menu-bar -- this is the
+    # "root" menu-item I have been raving about =)
+    menu_bar.append (root_menu)
+
+    # always display the window as the last step so it all splashes on
+    # the screen at once.
+    window.show()
+
+    # Respond to a button-press by posting a menu passed in as widget.
+    #
+    # Note that the "widget" argument is the menu being posted, NOT
+    # the button that was pressed.
+  def button_press(self, widget, event):
+    if event.type == gtk.gdk.BUTTON_PRESS:
+      widget.popup(None, None, None, event.button, event.time)
+      # Tell calling code that we have handled this event the buck
+      # stops here.
+      return True
+    # Tell calling code that we have not handled this event pass it on.
+    return False
+
+  # Print a string when a menu item is selected
+  def menuitem_response(self, widget, string):
+    print "%s" % string
 
 def main():
-    gtk.main()
-    return 0
+  gtk.main()
+  return 0
 
 if __name__ == "__main__":
-    ItemFactoryExample()
-    main()
+  MenuExample()
+  main()
