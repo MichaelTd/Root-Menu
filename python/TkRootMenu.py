277cbe54d37c656a12effd005754c27ec01fd07f
MichaelTd <tsouchlarakis@gmail.com>
2017-06-18 22:32:24 +0300

    Update

--- python/TkRootMenu.py
+++ python/TkRootMenu.py
@@ -15,19 +15,151 @@ def runCommand(app, prm=""):
   #(__out__, __err__) = proc.communicate()
   #print "program: " , app, " output: ", __out__, " error: ", __err__
 
+#__terminal__ = "xfce4-terminal --disable-server --geometry=120x40"
+__terminal__ = "terminology"
+#__editor__ = __terminal__ + " -e " + "micro"
+__editor__ = "nedit"
+__file_manager__ = "gentoo --root-ok"
+#__file_manager__ = "xfe"
+__browser__ = "firefox"
+#__sudo_cmd__ = "SUDO_ASKPASS=/usr/bin/ssh-askpass-fullscreen &&sudo --askpass "
+__sudo_cmd__ = "SUDO_ASKPASS=/usr/bin/x11-ssh-askpass sudo --askpass "
+
+basic_apps = (
+    ("Terminal", __terminal__, "ctl+t"),
+    ("Editor", __editor__, "ctl+e"),
+    ("File Manager", __file_manager__, "ctl+f"),
+    ("Browser", __browser__, "ctl+b"))
+
+net_apps = (
+    ("Firefox", "firefox"),
+    #("Firefox DE", "firefox-de"),
+    ("Seamonkey", "seamonkey"),
+    ("Tor Network", "cd /opt/tor/ && " + __terminal__ + " --login=false"),
+    ("Mail", "seamonkey -mail"),
+    #("FileZilla", "filezilla"),
+    ("W3m", __terminal__ + " -e w3m www.google.com"),
+    ("HexChat", "hexchat"))
+
+dev_apps = (
+    ("Eclipse", "eclipse"),
+    ("Netbeans", "netbeans"),
+    ("KDevelop", "kdevelop"),
+    #("QtCreator", "qtcreator.sh"),
+    #("QtDesigner", "qtchooser -run-tool=designer -qt=5"),
+    ("QtDesigner", "~/opt/qt/5.8/gcc_64/bin/designer"),
+    ("QtCreator", "~/opt/qt/Tools/QtCreator/bin/qtcreator.sh"),
+    ("Code Blocks", "codeblocks"),
+    ("Glade", "glade"),
+    ("Pg Admin3", "pgadmin3"),
+    ("Micro", __terminal__ + " -e " + "micro"),
+    ("Idle", "idle"),
+    ("Diakonos", __terminal__ + " -e " + "diakonos"),
+    ("Slap", __terminal__ + " -e " + "~/bin/slap"),
+    ("Vim", __terminal__ + " -e vim"),
+    #("Cuda Text", "${HOME}/bin/cudatext"),
+    ("NEdit", "nedit"),
+    ("Tea", "tea"),
+    ("GVim", "gvim"),
+    #("Xemacs", __terminal__ + " -e xemacs"),
+    #("Yudit", "yudit"),
+    #("JuPyter", __terminal__ + " -e jupyter notebook"),
+    #("ZED", __terminal__ + " -e /bin/env /bin/bash ~/opt/zed/zed"),
+    ("Emacs", "emacs"),
+    ("VStudio Code", "code"),
+    ("LightTable", "light"),
+    ("Sublime Text", "sublime"),
+    ("Atom", "atom"))
+
+media_apps = (
+    ("Open Office", "ooffice"),
+    ("Gimp", "gimp"),
+    #("Open Shot", "openshot"),
+    ("Blender", "/opt/blender"),
+    ("VLC", "vlc"),
+    ("Audacious", "audacious"),
+    ("Audacity", "audacity"))
+
+game_apps = (
+    ("GTypist", __terminal__ + " -e gtypist"),
+    ("KLavaro", "klavaro"),
+    ("GNU Back Gammon", "gnubg"),
+    ("XGammon", "xgammon"),
+    ("XBoard", "xboard"),
+    ("Xmahjongg","xmahjongg"),
+    ("X Mah-jongg","xmj"),
+    ("Quake 3", "~/bin/ioq3"),
+    ("Quake 3 TA", "~/bin/ioq3-ta"),
+    ("Urban Terror", "~/bin/ut42u"),
+    ("Warsow", "~/warsow_21/warsow"))
+
+fs_apps = (
+    ("Terminology", "terminology"),
+    ("Xfce4 Terminal", "xfce4-terminal --disable-server --geometry=120x40"),
+    ("URXVT", "urxvt"),
+    ("XTerm", "xterm"),
+    #("Hyper", "hyper"),
+    ("Midnight Commander", __terminal__ + " -e mc"),
+    #("Ranger", __terminal__ + " -e ranger"),
+    #("Rox filer", "rox"),
+    ("ViFm", __terminal__ + " -e vifm"),
+    ("Gentoo", "gentoo"),
+    ("Xfe", "xfe"),
+    ("SpaceFM", "spacefm"),
+    ("Thunar", "thunar"))
+
+admin_apps = (
+    ("Terminal", __sudo_cmd__ + " " + __terminal__),
+    ("Text Editor", __sudo_cmd__ + " " + __editor__),
+    ("File manager",  __sudo_cmd__ + " " + __file_manager__),
+    ("Midnight Commander", __sudo_cmd__ + " " + __terminal__ + " -e mc"),
+    #("Ranger", __sudo_cmd__ + " " + __terminal__ + " -e ranger"),
+    ("Porthole", __sudo_cmd__ + " porthole"),
+    ("Wireshark", __sudo_cmd__ + " " + __terminal__ + " -e wireshark"),
+    #("DStat", __sudo_cmd__ + " " + __terminal__ + " -e dstat -fcdngy"),
+    ("Glances", __sudo_cmd__ + " " + __terminal__ + " -e glances"),
+    ("PowerTop", __sudo_cmd__ + " " + __terminal__ + " -e powertop"),
+    ("HTop", __sudo_cmd__ + " " + __terminal__ + " -e htop"),
+    ("Top", __sudo_cmd__ + " " + __terminal__ + " -e top"))
+
+util_apps = (
+    ("App Runner", "~/bin/runcmd.sh"),
+    ("Xfce4 App Finder", "xfce4-appfinder --collapsed --disable-server"),
+    ("Xfce4 Screenshot", "xfce4-screenshooter"),
+    #("Screengrab", "screengrab"),
+    ("Take a shot now", "~/bin/imss.sh 2"),
+    ("Viewnior", "viewnior"),
+    #("PeaZip", "peazip"),
+    ("Xarchiver", "xarchiver"),
+    ("Ghost View", "gv"),
+    ("Foxit Reader", "foxitreader"),
+    ("Xv", "xv"),
+    ("jCalculator", "jCalculator.sh"),
+    ("jsCalculator", __browser__ + " ~/git/fcc-app/01-front-end-cert/07-javascript-calculator/jc.html"),
+    ("Calculator", "calculator"))
+
+config_apps = (
+    ("Compiz settings manager", "ccsm"),
+    ("Emerald themes manager", "emerald-theme-manager"),
+    ("Conky config", __editor__ + " ~/.conky.conf/"),
+    ("Edit backup files", __editor__ + " ~/.backup.txt"),
+    ("Edit shell files", __editor__ + " ~/.zsh* ~/.bash* /etc/bash/bashrc.d/*.sh "),
+    ("View Log files", __editor__ + " /var/log/"),
+    ("X Screen Saver", "xscreensaver-demo"),
+    #("Xfce4-alsa-control", "xfce4-alsa-control"),
+    #("Alsamixer Gui", "alsamixergui"),
+    ("Alsamixer", __terminal__ + " -e alsamixer"),
+    ("Volume Prefs", "paprefs"),
+    ("Volume Controls", "pavucontrol"))
+
+pc_options = (
+    ("Logout", "kill -15 -1"),
+    ("Reboot", __sudo_cmd__ + " shutdown -r now"),
+    ("Shutdown", __sudo_cmd__ + " shutdown -h now"))
+
 class TkRootMenu(Tk):
 
     def __init__(self, master):
-
-        #self.__terminal__ = "xfce4-terminal --disable-server --geometry=120x40"
-        self.__terminal__ = "terminology"
-        #self.__editor__ = self.__terminal__ + " -e " + "micro"
-        self.__editor__ = "nedit"
-        self.__file_manager__ = "gentoo --root-ok"
-        #self.__file_manager__ = "xfe"
-        self.__browser__ = "firefox"
-        #self.__sudo_cmd__ = "SUDO_ASKPASS=/usr/bin/ssh-askpass-fullscreen &&sudo --askpass "
-        self.__sudo_cmd__ = "SUDO_ASKPASS=/usr/bin/x11-ssh-askpass sudo --askpass "
 
         self.master = master
         self.master.title("Root Menu")
@@ -59,170 +191,52 @@ class TkRootMenu(Tk):
         menubar.add_cascade(label="Root Menu", menu=appsmenu)
 
         # Basic apps
-        for lbl, cmmnd, k in (
-            ("Terminal", self.__terminal__, "ctl+t"),
-            ("Editor", self.__editor__, "ctl+e"),
-            ("File Manager", self.__file_manager__, "ctl+f"),
-            ("Browser", self.__browser__, "ctl+b")):
+        for lbl, cmmnd, k in basic_apps:
             appsmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param), accelerator=k)
 
         appsmenu.add_separator()
 
         # Internet
         netmenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("Firefox", "firefox"),
-            #("Firefox DE", "firefox-de"),
-            ("Seamonkey", "seamonkey"),
-            ("Tor Network", "cd /opt/tor/ && " + self.__terminal__ + " --login=false"),
-            ("Mail", "seamonkey -mail"),
-            #("FileZilla", "filezilla"),
-            ("W3m", self.__terminal__ + " -e w3m www.google.com"),
-            ("HexChat", "hexchat")):
+        for lbl, cmmnd in net_apps:
             netmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
         # Dev menu
         devmenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("Eclipse", "eclipse"),
-            ("Netbeans", "netbeans"),
-            ("KDevelop", "kdevelop"),
-            #("QtCreator", "qtcreator.sh"),
-            #("QtDesigner", "qtchooser -run-tool=designer -qt=5"),
-            ("QtDesigner", "~/opt/qt/5.8/gcc_64/bin/designer"),
-            ("QtCreator", "~/opt/qt/Tools/QtCreator/bin/qtcreator.sh"),
-            ("Code Blocks", "codeblocks"),
-            ("Glade", "glade"),
-            ("Pg Admin3", "pgadmin3"),
-            ("Micro", self.__terminal__ + " -e " + "micro"),
-            ("Idle", "idle"),
-            ("Diakonos", self.__terminal__ + " -e " + "diakonos"),
-            ("Slap", self.__terminal__ + " -e " + "~/bin/slap"),
-            ("Vim", self.__terminal__ + " -e vim"),
-            #("Cuda Text", "${HOME}/bin/cudatext"),
-            ("NEdit", "nedit"),
-            ("Tea", "tea"),
-            ("GVim", "gvim"),
-            ("Emacs", "emacs"),
-            ("VStudio Code", "code"),
-            ("LightTable", "light"),
-            ("Sublime Text", "sublime"),
-            ("Atom", "atom")):
-            #("Xemacs", self.__terminal__ + " -e xemacs"),
-            #("Yudit", "yudit"),
-            #("JuPyter", self.__terminal__ + " -e jupyter notebook"),
-            #("ZED", self.__terminal__ + " -e /bin/env /bin/bash ~/opt/zed/zed"),
+        for lbl, cmmnd in dev_apps:
             devmenu.add_command(label = lbl,command = lambda param = cmmnd: runCommand(param))
 
         # Multimedia
         mmmenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("Open Office", "ooffice"),
-            ("Gimp", "gimp"),
-            #("Open Shot", "openshot"),
-            ("Blender", "/opt/blender"),
-            ("VLC", "vlc"),
-            ("Audacious", "audacious"),
-            ("Audacity", "audacity")):
+        for lbl, cmmnd in media_apps:
             mmmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
         # Games menu
         gammenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("GTypist", self.__terminal__ + " -e gtypist"),
-            ("KLavaro", "klavaro"),
-            ("GNU Back Gammon", "gnubg"),
-            ("XGammon", "xgammon"),
-            ("XBoard", "xboard"),
-            ("Xmahjongg","xmahjongg"),
-            ("X Mah-jongg","xmj"),
-            ("Quake 3", "~/bin/ioq3"),
-            ("Quake 3 TA", "~/bin/ioq3-ta"),
-            ("Urban Terror", "~/bin/ut42u"),
-            ("Warsow", "~/warsow_21/warsow")):
+        for lbl, cmmnd in game_apps:
             gammenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
         # File system tools
         toolsmenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("Terminology", "terminology"),
-            ("Xfce4 Terminal", "xfce4-terminal --disable-server --geometry=120x40"),
-            ("URXVT", "urxvt"),
-            ("XTerm", "xterm"),
-            #("Hyper", "hyper"),
-            ("Midnight Commander", self.__terminal__ + " -e mc"),
-            #("Ranger", self.__terminal__ + " -e ranger"),
-            #("Rox filer", "rox"),
-            ("ViFm", self.__terminal__ + " -e vifm"),
-            ("Gentoo", "gentoo"),
-            ("Xfe", "xfe"),
-            ("SpaceFM", "spacefm"),
-            ("Thunar", "thunar")):
+        for lbl, cmmnd in fs_apps:
             toolsmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
         # Admin tools
         adminmenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("Terminal", self.__sudo_cmd__ + " " + self.__terminal__),
-            ("Text Editor", self.__sudo_cmd__ + " " + self.__editor__),
-            ("File manager",  self.__sudo_cmd__ + " " + self.__file_manager__),
-            ("Midnight Commander", self.__sudo_cmd__ + " " + self.__terminal__ + " -e mc"),
-            #("Ranger", self.__sudo_cmd__ + " " + self.__terminal__ + " -e ranger"),
-            ("Porthole", self.__sudo_cmd__ + " porthole"),
-            ("Wireshark", self.__sudo_cmd__ + " " + self.__terminal__ + " -e wireshark"),
-            #("DStat", self.__sudo_cmd__ + " " + self.__terminal__ + " -e dstat -fcdngy"),
-            ("Glances", self.__sudo_cmd__ + " " + self.__terminal__ + " -e glances"),
-            ("PowerTop", self.__sudo_cmd__ + " " + self.__terminal__ + " -e powertop"),
-            ("HTop", self.__sudo_cmd__ + " " + self.__terminal__ + " -e htop"),
-            ("Top", self.__sudo_cmd__ + " " + self.__terminal__ + " -e top")):
+        for lbl, cmmnd in admin_apps:
             adminmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
         # Util Menu
         utilmenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("App Runner", "~/bin/runcmd.sh"),
-            ("Xfce4 App Finder", "xfce4-appfinder --collapsed --disable-server"),
-            ("Xfce4 Screenshot", "xfce4-screenshooter"),
-            #("Screengrab", "screengrab"),
-            ("Take a shot now", "~/bin/imss.sh 2"),
-            ("Viewnior", "viewnior"),
-            #("PeaZip", "peazip"),
-            ("Xarchiver", "xarchiver"),
-            ("Ghost View", "gv"),
-            ("Foxit Reader", "foxitreader"),
-            ("Xv", "xv"),
-            ("jCalculator", "jCalculator.sh"),
-            ("jsCalculator", self.__browser__ + " ~/git/fcc-app/01-front-end-cert/07-javascript-calculator/jc.html"),
-            ("Calculator", "calculator")):
+        for lbl, cmmnd in util_apps:
             utilmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
         # Config Menu
         configmenu = Menu(menubar)
-
-        for lbl, cmmnd in (
-            ("Compiz settings manager", "ccsm"),
-            ("Emerald themes manager", "emerald-theme-manager"),
-            ("Conky config", self.__editor__ + " ~/.conky.conf/"),
-            ("Edit backup files", self.__editor__ + " ~/.backup.txt"),
-            ("Edit shell files", self.__editor__ + " ~/.zsh* ~/.bash* /etc/bash/bashrc.d/*.sh "),
-            ("View Log files", self.__editor__ + " /var/log/"),
-            ("X Screen Saver", "xscreensaver-demo"),
-            ("Alsamixer", self.__terminal__ + " -e alsamixer"),
-            ("Volume Prefs", "paprefs"),
-            ("Volume Controls", "pavucontrol")):
-            #("Xfce4-alsa-control", "xfce4-alsa-control"),
-            #("Alsamixer Gui", "alsamixergui"),
+        for lbl, cmmnd in config_apps:
             configmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
-        # Groups
-        for lbl, mnGrp in (
+        self.groups = (
             ("Internet", netmenu),
             ("Development", devmenu),
             ("Mediums", mmmenu),
@@ -230,24 +244,23 @@ class TkRootMenu(Tk):
             ("File System", toolsmenu),
             ("Admin Tools", adminmenu),
             ("Utilities", utilmenu),
-            ("Config", configmenu)):
+            ("Config", configmenu))
+
+        # Groups
+        for lbl, mnGrp in self.groups:
             appsmenu.add_cascade(label=lbl,menu=mnGrp)
 
         appsmenu.add_separator()
 
         # Menu
-        #appsmenu.add_command(label="Edit Menu", command=lambda: runCommand("idle2.7" + " /usr/local/bin/tkRootMenu.py"))
-        appsmenu.add_command(label="Edit Menu", command=lambda: runCommand(self.__editor__ + " " + sys.argv[0]))
+        appsmenu.add_command(label="Edit Menu", command=lambda: runCommand(__editor__ + " " + sys.argv[0]))
         appsmenu.add_command(label="Refresh Menu", command=self.runRfs)
         appsmenu.add_command(label="Close Menu", command=self.master.quit)
 
         appsmenu.add_separator()
 
         # PC
-        for lbl, cmmnd in (
-            ("Logout", "kill -15 -1"),
-            ("Reboot", self.__sudo_cmd__ + " shutdown -r now"),
-            ("Shutdown", self.__sudo_cmd__ + " shutdown -h now")):
+        for lbl, cmmnd in pc_options:
             appsmenu.add_command(label=lbl,command=lambda param=cmmnd: runCommand(param))
 
         self.master.config(menu=menubar)
@@ -261,13 +274,13 @@ class TkRootMenu(Tk):
 
     # Default Apps
     def on_accel_runTerminal(self, widget):
-        runCommand(self.__terminal__)
+        runCommand(__terminal__)
     def on_accel_runEditor(self, widget):
-        runCommand(self.__editor__)
+        runCommand(__editor__)
     def on_accel_runFileManager(self, widget):
-        runCommand(self.__file_manager__)
+        runCommand(__file_manager__)
     def on_accel_runBrowser(self, widget):
-        runCommand(self.__browser__)
+        runCommand(__browser__)
 
     # Volume
     def setVlm(self, widget):
